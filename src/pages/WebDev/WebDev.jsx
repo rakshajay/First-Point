d9b8d6bcd366f2d8c8cf68aed541f965ee5b7b2a
rakshajay <rakshashettyhs@gmail.com>
2024-11-09 18:33:57 -0800

    link to github added on main image of webdev

--- src/pages/WebDev/WebDev.jsx
+++ src/pages/WebDev/WebDev.jsx
@@ -16,20 +16,19 @@ const webdev = () => {
         // Make an Axios call to the backend
         const response = await axios.get(`${serverURL}/webdev`);
         setProjects(response.data);
-        console.log("response.data", response.data)
         setLoading(false);
+        console.log(response.data)
       } catch (err) {
         console.error('Error fetching projects:', err);
         setError('Error fetching projects data');
         setLoading(false);
       }
     };
-
+ 
     fetchProjects();
   }, [serverURL]);
 
-  if (loading) return <div>Loading...</div>;
-  if (error) return <div>{error}</div>;
+  
 
   return (
     <div className="webdev-container">
